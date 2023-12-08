92c81cd58efecafa20fab1f550515762584f094e
letitiacarciu <letitialeti@gmail.com>
2023-12-08 16:10:32 +0100

    test iMac

--- src/test/java/HelpMethods/AlertsMethods.java
+++ src/test/java/HelpMethods/AlertsMethods.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-
+//test
 public class AlertsMethods {
     private WebDriver driver;
 
